4153b9ab2b1933d548744e457925970679c20a76
Alexander <san.juckov2017@yandex.ru>
2024-03-13 16:25:32 +0300

    Fix file models.py

--- catalog/models.py
+++ catalog/models.py
@@ -6,7 +6,7 @@ NULLABLE = {'blank': True, 'null': True}
 
 class Category(models.Model):
     name_category = models.CharField(max_length=100, verbose_name='Наименование')
-    description_category = models.TextField(max_length=200, verbose_name='Описание')
+    description_category = models.TextField(max_length=200, verbose_name='Описание', blank=True, null=True)
 
     def __str__(self):
         return f'{self.name_category}'
@@ -17,15 +17,14 @@ class Category(models.Model):
         ordering = ('name_category',)
 
 
-class Catalog(models.Model):
+class Products(models.Model):
     name_product = models.CharField(max_length=100, verbose_name='Наименование')
-    description_product = models.TextField(max_length=200, verbose_name='Описание')
+    description_product = models.TextField(max_length=200, verbose_name='Описание', **NULLABLE)
     image = models.ImageField(upload_to='img_catalogs/', verbose_name='Изображение(превью)', **NULLABLE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
     price = models.IntegerField(verbose_name='Цена за покупку')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
-    manufactured_at = models.DateField(auto_now_add=True, verbose_name='Дата производства продукта')
 
     def __str__(self):
         return f'{self.name_product}'
