3ff19b05154849570ace283f13979ac78b576a9d
Alexander <san.juckov2017@yandex.ru>
2024-03-20 17:41:18 +0300

    Add file product_detail.html and update file's views.py and urls.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Products
 
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    products = Products.objects.all()
+    return render(request, 'catalog/home.html', {'products': products})
 
 
 def contact(request):
@@ -12,4 +14,12 @@ def contact(request):
         message = request.POST.get('message')
         print(f'Пользователь с именем: {name}\nДля связи оставил телефон: {phone}\nОставил сообщение: {message}')
     return render(request, 'catalog/contact.html')
-# Create your views here.
+
+
+def product_detail(request, pk):
+    # Получаем объект товара по его PrimaryKey (id)
+    product = Products.objects.get(pk=pk)
+    # Формируем контекст для передачи в шаблон
+    context = {'product': product}
+    # Отображаем страницу с информацией о товаре
+    return render(request, 'catalog/product_detail.html', context)
