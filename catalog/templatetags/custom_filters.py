82da2877bbf78a8d0802174917d034c6ba85605a
Alexander <san.juckov2017@yandex.ru>
2024-03-20 17:49:34 +0300

    Add file custom_filters.py

---
+++ catalog/templatetags/custom_filters.py
@@ -0,0 +1,10 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def media_url(image_path):
+    if image_path:
+        return f'/media/{image_path}'
+    return ''
