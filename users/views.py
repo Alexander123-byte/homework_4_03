596c93383b82737aaa54cdf7f2b27d32bbcf13ef
Alexander <san.juckov2017@yandex.ru>
2024-04-19 15:51:30 +0300

    Adding a password recovery feature, as well as adding templates for the 'users' application

--- users/views.py
+++ users/views.py
@@ -1,9 +1,14 @@
 import secrets
+import random
 
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
 from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404, redirect
-from django.views.generic import CreateView
+from django.views.generic import CreateView, FormView
 from django.urls import reverse_lazy, reverse
+from django.conf import settings
 
 from users.forms import UserRegisterForm
 from users.models import User
@@ -37,3 +42,30 @@ def email_verification(request, token):
     user.is_active = True
     user.save()
     return redirect(reverse("users:login"))
+
+
+class EmailForm(forms.Form):
+    email = forms.EmailField(label='Email')
+
+
+class PasswordResetView(FormView):
+    template_name = 'users/password_reset.html'
+    form_class = EmailForm
+    success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        email = form.cleaned_data['email']
+        new_password = ''.join(random.choices(settings.PASSWORD_CHARS, k=settings.PASSWORD_LENGTH))
+
+        user = get_user_model().objects.get(email=email)
+        user.password = make_password(new_password)
+        user.save()
+
+        send_mail(
+            'Восстановление пароля',
+            f'Ваш новый пароль: {new_password}',
+            settings.EMAIL_HOST_USER,
+            [email],
+            fail_silently=False,
+        )
+        return super().form_valid(form)
